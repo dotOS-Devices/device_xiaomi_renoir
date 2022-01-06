c281257011a96f9d60c6ffb3b30b35c2af069cf4
IacobIonut01 <paulionut2003@gmail.com>
2022-01-06 21:04:01 +0000

    renoir: PixelExperience bring-up
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- aosp_renoir.mk
+++ aosp_renoir.mk
@@ -12,19 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
 
-# Inherit some common ProjectSrak stuff.
-$(call inherit-product, vendor/streak/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := streak_renoir
+PRODUCT_NAME := aosp_renoir
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
-# Inherit srak official
-STREAK_BUILD_TYPE=OFFICIAL
+WITH_GAPPS := true
+USE_CCACHE := true
